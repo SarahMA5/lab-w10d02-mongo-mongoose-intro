5bb9de09f0dbd070cd5899b7a76e605ecf25da8a
Sarah <alahmadisr@gmail.com>
2020-01-21 10:52:22 +0300

    hw

--- models/vampire.js
+++ models/vampire.js
@@ -2,10 +2,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const vampireSchema = new Schema({
-  // add your code here to set up your schema
+
+ 
+    name: { type: String, required: true}, //name field is required
+    title: String,
+    hair_color: {type: String, default: "blonde"}, //default value of the hair color to blonde.
+    eye_color: String,
+    dob: Date,
+    loves: [{type: String}],
+    location: String,
+    gender: String,
+    victims: {type:Number, min: 0} //no vampire will have less than 0 victims
+  
 });
 
 const Vampire = mongoose.model('Vampire', vampireSchema);
+
+
+
+
 
 //make this exportable to be accessed in `app.js`
 module.exports = Vampire;
